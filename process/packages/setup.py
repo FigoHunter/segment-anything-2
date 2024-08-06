1170599e0ce63c23b04c5e40b003f96a642d7702
zhaoyifei <zhaoyifei19950308@hotmail.com>
2024-08-06 07:14:24 +0000

    process

---
+++ process/packages/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name='sam_process',
+    version='0.1.0',
+    packages=find_packages(),
+    license='MIT',
+    author='figo',
+    description='figo',
+    install_requires=[],
+    tests_require=[]
+)
